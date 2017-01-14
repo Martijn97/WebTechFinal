c4241e0d2dd040e4ed8566ac035fb24591808553
156784 <julia.hofs@gmail.com>
2017-01-14 16:06:32 +0100

    Routing fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 // const Post = require('./db/models/posts');
 
@@ -12,6 +13,11 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(__dirname + '/client/build'));
 }
+
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+})
+
 
 // const database = 'mongodb://username:password@databaseurl';
 // const db = mongoose.createConnection(database);

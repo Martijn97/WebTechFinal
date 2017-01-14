31b7f7b4fec29a0ebede200cac1b4cec5e3fca11
156784 <julia.hofs@gmail.com>
2017-01-14 23:28:44 +0100

    Adding files from branch julia to master

---
+++ client/src/components/Index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Row, Col, Button, Icon} from 'react-materialize';
+
+export default class Index extends React.Component {
+
+	render() {
+
+		return (
+			<div>
+				<Row>
+					<Col s={12} style={{"textAlign": "center"}}><h1 className="teal-text">Welcome to Instagram world!</h1></Col>
+				</Row>
+				<Row>
+					<Col s={12} style={{"textAlign": "center"}}>
+						<a href="https://api.instagram.com/oauth/authorize/?client_id=1159204fb5b94378904fa06932f07da6&redirect_uri=http://webtechnologytue.herokuapp.com/home&response_type=token">
+							<Button style={{"textAlign": "center"}} className="teal">Authenticate<Icon>person</Icon></Button>
+						</a>
+					</Col>
+				</Row>
+			</div>
+		);
+	}
+};

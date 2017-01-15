09a1fa86a00b09d598a8392b5b1588e9d5f427dc
156784 <julia.hofs@gmail.com>
2017-01-15 19:00:24 +0100

    cors headers express

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const app = express();
 app.set('port', (process.env.PORT || 3001));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 // Express only serves static assets in production

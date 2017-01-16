ab2b4ae8839964dd40ebc5985699ba4a2d9d959f
156784 <julia.hofs@gmail.com>
2017-01-16 13:06:29 +0100

    test access code

--- server.js
+++ server.js
@@ -33,24 +33,26 @@ app.get('/insta_auth', (req, res) => {
 	let accessCode = req.query.code;
 	console.log(accessCode);
 
-	request.post('https://api.instagram.com/oauth/access_token',
-	{
-		form: {
-			client_id: '1159204fb5b94378904fa06932f07da6',
-			client_secret: '07dbbb23552d4c77929cd70986bbe8a3',
-			grant_type: 'authorization_code',
-			redirect_uri: 'http://webtechnologytue.herokuapp.com/insta_auth',
-			code: accessCode
-		}
-	}, (err, response, body) => {
-		if (err) {
-			console.log("Error in posting", err);
-		} else {
-			console.log(response);
-			console.log(body.json());
-			res.send(response);
-		}
-	})
+	res.send(accessCode);
+
+	// request.post('https://api.instagram.com/oauth/access_token',
+	// {
+	// 	form: {
+	// 		client_id: '1159204fb5b94378904fa06932f07da6',
+	// 		client_secret: '07dbbb23552d4c77929cd70986bbe8a3',
+	// 		grant_type: 'authorization_code',
+	// 		redirect_uri: 'http://webtechnologytue.herokuapp.com/insta_auth',
+	// 		code: accessCode
+	// 	}
+	// }, (err, response, body) => {
+	// 	if (err) {
+	// 		console.log("Error in posting", err);
+	// 	} else {
+	// 		console.log(response);
+	// 		console.log(body.json());
+	// 		res.send(response);
+	// 	}
+	// })
 });
 
 
